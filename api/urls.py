8a2dfff7b4db2fb141e98181e0a0f07221843348
Phelyppe Alex <phelyppesilva@gmail.com>
2024-10-25 16:50:33 -0300

    utilizando generics views

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from . import views
+from mini_twitter_app.views import *
 
 urlpatterns = [
-    path('', views.getPosts),
+    path('post/', PostListCreateAPIView.as_view(), name='post-create-list'),
+    path('post/<int:pk>/', PostRetrieveDestroyAPIView.as_view(), name='post-detail-view'),
+
+    path('person/', PersonCreateListView.as_view(), name='person-create-list'),
+    path('person/<int:pk>/', PersonRetrieveDestroyAPIView.as_view(), name='person-detail-view'),
+    
+    path('like/', LikeListCreateAPIView.as_view(), name='like-create-list'),
+    path('like/<int:pk>/', LikeRetrieveDestroyAPIView.as_view(), name='like-detail-view'),
 ]
--- mini_twitter_app/views.py
+++ mini_twitter_app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import generics
+from mini_twitter_app.models import *
+from api.serializers import *
 
-# Create your views here.
+# PERSON
+
+class PersonCreateListView(generics.ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer = PersonSerializer(queryset, many=True)
+
+class PersonRetrieveDestroyAPIView(generics.RetrieveDestroyAPIView):
+    queryset = Person.objects.all()
+    serializer = PersonSerializer(queryset, many=True)
+
+# POST
+
+class PostListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer = PostSerializer(queryset, many=True)
+
+class PostRetrieveDestroyAPIView(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer = PostSerializer(queryset, many=True)
+
+# LIKE
+
+class LikeListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Like.objects.all()
+    serializer = LikeSerializer(queryset, many=True)
+
+class LikeRetrieveDestroyAPIView(generics.RetrieveDestroyAPIView):
+    queryset = Like.objects.all()
+    serializer = LikeSerializer(queryset, many=True)
